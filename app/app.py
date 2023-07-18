b8f8436bdee9381806c410bf4cf5a322ea1585cf
mjkareem <mujahidakande@gmail.com>
2023-07-19 01:37:33 +0200

    attempting to deploy

--- app/app.py
+++ app/app.py
@@ -1,18 +1,15 @@
-#import calendar  # Core Python Module
-#from datetime import datetime  # Core Python Module
+import plotly.graph_objects as go
+import streamlit as st
+from streamlit_option_menu import option_menu
 
-import plotly.graph_objects as go  # pip install plotly
-import streamlit as st  # pip install streamlit
-from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
-
-import database as db  # local import
+import database as db
 
 # -------------- SETTINGS --------------
 finalPrice = ["Cold Price", "Warm Price", "Other Price (eg: Insurance)"]
 mainFacilities = ["Parking", "Balcony", "Elevator", "Garden", "Cellar"]
 currency = "EUR"
 page_title = "House Prices in Berlin Logger"
-page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+page_icon = ":money_with_wings:"
 layout = "centered"
 # --------------------------------------
 
@@ -20,34 +17,44 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
 # --- DROP DOWN VALUES FOR SELECTING THE BERLIN DISTRICTS ---
-districts = ["Mitte", "Friedrichshain-Kreuzberg", "Pankow", "Charlottenburg-Wilmersdorf", "Spandau", "Steglitz-Zehlendorf", "Tempelhof-Schöneberg", "Neukölln", "Treptow-Köpenick", "Marzahn-Hellersdorf", "Lichtenberg", "Reinickendorf"]
-#codes = list(range(10115, 14199))
+districts = ["Mitte", "Friedrichshain-Kreuzberg", "Pankow", "Charlottenburg-Wilmersdorf", "Spandau", "Steglitz-Zehlendorf",
+             "Tempelhof-Schöneberg", "Neukölln", "Treptow-Köpenick", "Marzahn-Hellersdorf", "Lichtenberg", "Reinickendorf"]
 codes = [
     ("10115", "Mitte"), ("10117", "Mitte"), ("10119", "Mitte"), ("10178", "Mitte"), ("10179", "Mitte"),
     ("10243", "Friedrichshain"), ("10245", "Friedrichshain"), ("10247", "Friedrichshain"), ("10249", "Friedrichshain"),
     ("10318", "Lichtenberg"), ("10319", "Lichtenberg"),
-    ("10405", "Prenzlauer Berg"), ("10407", "Prenzlauer Berg"), ("10409", "Prenzlauer Berg"), ("10435", "Prenzlauer Berg"), ("10437", "Prenzlauer Berg"), ("10439", "Prenzlauer Berg"),
-    ("10585", "Charlottenburg"), ("10587", "Charlottenburg"), ("10589", "Charlottenburg"), ("10623", "Charlottenburg"), ("10625", "Charlottenburg"), ("10627", "Charlottenburg"), ("10629", "Charlottenburg"),
+    ("10405", "Prenzlauer Berg"), ("10407", "Prenzlauer Berg"), ("10409", "Prenzlauer Berg"), ("10435", "Prenzlauer Berg"),
+    ("10437", "Prenzlauer Berg"), ("10439", "Prenzlauer Berg"),
+    ("10585", "Charlottenburg"), ("10587", "Charlottenburg"), ("10589", "Charlottenburg"), ("10623", "Charlottenburg"),
+    ("10625", "Charlottenburg"), ("10627", "Charlottenburg"), ("10629", "Charlottenburg"),
     ("10823", "Schöneberg"), ("10825", "Schöneberg"), ("10827", "Schöneberg"), ("10829", "Schöneberg"),
-    ("10961", "Kreuzberg"), ("10963", "Kreuzberg"), ("10965", "Kreuzberg"), ("10967", "Kreuzberg"), ("10969", "Kreuzberg"), ("10997", "Kreuzberg"), ("109979", "Kreuzberg"),
-    ("12043", "Neukölln"), ("12045", "Neukölln"), ("12047", "Neukölln"), ("12049", "Neukölln"), ("12051", "Neukölln"), ("12053", "Neukölln"), ("12055", "Neukölln"), ("12057", "Neukölln"), ("12059", "Neukölln"),
+    ("10961", "Kreuzberg"), ("10963", "Kreuzberg"), ("10965", "Kreuzberg"), ("10967", "Kreuzberg"), ("10969", "Kreuzberg"),
+    ("10997", "Kreuzberg"), ("109979", "Kreuzberg"),
+    ("12043", "Neukölln"), ("12045", "Neukölln"), ("12047", "Neukölln"), ("12049", "Neukölln"), ("12051", "Neukölln"),
+    ("12053", "Neukölln"), ("12055", "Neukölln"), ("12057", "Neukölln"), ("12059", "Neukölln"),
     ("12203", "Steglitz"), ("12205", "Steglitz"), ("12207", "Steglitz"), ("12209", "Steglitz"),
     ("12247", "Zehlendorf"), ("12249", "Zehlendorf"), ("12277", "Zehlendorf"), ("12279", "Zehlendorf"),
-    ("12305", "Mariendorf"), ("12307", "Mariendorf"), ("12309", "Mariendorf"), ("12487", "Mariendorf"), ("12489", "Mariendorf"),
-    ("12555", "Pankow"), ("12557", "Pankow"), ("12559", "Pankow"), ("12679", "Pankow"), ("12681", "Pankow"), ("12683", "Pankow"), ("12685", "Pankow"), ("12687", "Pankow"), ("12689", "Pankow"),
-    ("13086", "Reinickendorf"), ("13088", "Reinickendorf"), ("13089", "Reinickendorf"), ("13187", "Reinickendorf"), ("13189", "Reinickendorf"), ("13347", "Reinickendorf"), ("13349", "Reinickendorf"), 
-    ("13351", "Spandau"), ("13353", "Spandau"), ("13355", "Spandau"), ("13357", "Spandau"), ("13359", "Spandau"), ("13403", "Spandau"), ("13405", "Spandau"), ("13407", "Spandau"), ("13409", "Spandau"),
+    ("12305", "Mariendorf"), ("12307", "Mariendorf"), ("12309", "Mariendorf"), ("12487", "Mariendorf"),
+    ("12489", "Mariendorf"),
+    ("12555", "Pankow"), ("12557", "Pankow"), ("12559", "Pankow"), ("12679", "Pankow"), ("12681", "Pankow"),
+    ("12683", "Pankow"), ("12685", "Pankow"), ("12687", "Pankow"), ("12689", "Pankow"),
+    ("13086", "Reinickendorf"), ("13088", "Reinickendorf"), ("13089", "Reinickendorf"), ("13187", "Reinickendorf"),
+    ("13189", "Reinickendorf"), ("13347", "Reinickendorf"), ("13349", "Reinickendorf"),
+    ("13351", "Spandau"), ("13353", "Spandau"), ("13355", "Spandau"), ("13357", "Spandau"), ("13359", "Spandau"),
+    ("13403", "Spandau"), ("13405", "Spandau"), ("13407", "Spandau"), ("13409", "Spandau"),
     ("13435", "Treptow"), ("13437", "Treptow"), ("13439", "Treptow"), ("14109", "Treptow"),
     ("14163", "Köpenick"), ("14165", "Köpenick"), ("14167", "Köpenick"), ("14169", "Köpenick")
 ]
 
 
-
-
 # --- DATABASE INTERFACE ---
 def get_all_berlinDistricts():
+    """Returns a list of all Berlin districts"""
     items = db.fetch_all_berlinDistricts()
-    berlinDistricts = [item["berlinDistrict"] for item in items]
+    berlinDistricts = []
+    for item in items:
+        if "berlinDistricts" in item:
+            berlinDistricts.append(item["berlinDistricts"])
     return berlinDistricts
 
 
@@ -100,42 +107,29 @@ if selected == "Data Entry":
             db.insert_berlinDistrict(berlinDistrict, finalPrice, mainFacilities, comment)
             st.success("Data saved!")
 
-
-# --- PLOT BERLIN DISTRICTS ---
-if selected == "Data Visualization":
+# --- DATA VISUALIZATION ---
+elif selected == "Data Visualization":
     st.header("Data Visualization")
     with st.form("saved_berlinDistricts"):
-        berlinDistrict = st.selectbox("Select District:", get_all_berlinDistricts())
-        submitted = st.form_submit_button("Plot District Prices")
+        berlinDistricts = get_all_berlinDistricts()
+        submitted = st.form_submit_button("Plot Prices")
         if submitted:
-            # Get data from database
-            berlinDistrict_data = db.get_berlinDistrict(berlinDistrict)
-            comment = berlinDistrict_data.get("comment")
-            mainFacilities = berlinDistrict_data.get("mainFacilities")
-            finalPrice = berlinDistrict_data.get("finalPrice")
+            if berlinDistricts:
+                st.subheader("Select Berlin District:")
+                selected_district = st.selectbox("Districts", berlinDistricts)
+                data = db.fetch_berlinDistrict_data(selected_district)
+                if data:
+                    st.subheader("Total Price")
+                    fig_total_price = go.Figure(data=[go.Bar(x=data["Dates"], y=data["Total Price"])])
+                    fig_total_price.update_layout(title="Total Price Over Time")
+                    st.plotly_chart(fig_total_price)
 
-            # Create metrics
-            total_total_prices = sum(finalPrice.values())
-            total_mainFacilities = sum(mainFacilities.values())
-            #remaining_budget = total_income - total_expense
-            col1, col2 = st.columns(2)
-            col1.metric("Total Price", f"{total_total_prices} {currency}")
-            col2.metric("Total Main Facilities", total_mainFacilities)
-            #col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
-            st.text(f"Comment: {comment}")
+                    st.subheader("Main Facilities")
+                    fig_main_facilities = go.Figure(data=[go.Scatter(x=data["Dates"], y=data[facility], mode="markers") for facility in mainFacilities])
+                    fig_main_facilities.update_layout(title="Main Facilities Over Time")
+                    st.plotly_chart(fig_main_facilities)
+                else:
+                    st.warning("No data available for the selected district.")
+            else:
+                st.warning("No Berlin districts available.")
 
-            # Create sankey chart
-            label = list(finalPrice.keys()) + ["Total Price"] + list(mainFacilities.keys())
-            source = list(range(len(finalPrice))) + [len(finalPrice)] * len(mainFacilities)
-            target = [len(finalPrice)] * len(finalPrice) + [label.index(main_facilities) for main_facilities in main_facilities.keys()]
-            value = list(finalPrice.values()) + list(main_facilities.values())
-
-            # Data to dict, dict to sankey
-            link = dict(source=source, target=target, value=value)
-            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
-            data = go.Sankey(link=link, node=node)
-
-            # Plot it!
-            fig = go.Figure(data)
-            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
-            st.plotly_chart(fig, use_container_width=True)
--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
-import streamlit as st  # pip install streamlit
-from deta import Deta  # pip install deta
-
+import streamlit as st
+from deta import Deta
 
 # Load the environment variables
 DETA_KEY = "a0mnukwqayt_cLiQRjhxBc1SyDRRxLyk84Vxp2np6GW2"
@@ -14,13 +13,31 @@ db = deta.Base("housing_berlin")
 
 def insert_berlinDistrict(berlinDistrict, finalPrice, mainFacilities, comment):
     """Returns the report on a successful creation, otherwise raises an error"""
-    return db.put({"key": berlinDistrict, "finalPrice": finalPrice, "mainFacilities": mainFacilities, "comment": comment})
+    data = {
+        "berlinDistricts": berlinDistrict,
+        "finalPrice": finalPrice,
+        "mainFacilities": mainFacilities,
+        "comment": comment
+    }
+    return db.put(data)
+
+def get_all_berlinDistricts():
+    """Returns a list of all Berlin districts"""
+    res = db.fetch()
+    berlinDistricts = [item.get("berlinDistricts") for item in res.items]
+    return berlinDistricts
 
 
 def fetch_all_berlinDistricts():
-    """Returns a dict of all periods"""
+    """Returns a dict of all Berlin districts"""
     res = db.fetch()
     return res.items
+
+
+def fetch_berlinDistrict_data(berlinDistrict):
+    """Returns the data for a specific Berlin district"""
+    data = db.get(berlinDistrict)
+    return data
 
 
 def get_berlinDistrict(berlinDistrict):
